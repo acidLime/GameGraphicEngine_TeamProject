48e5fa430b356b1cfcb3d9bae398f05262a5d49b
겜준 <@.>
2019-12-18 23:26:04 +0900

    *아우라 라이트 컨트롤러

---
+++ Assets/AuraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AuraAPI;
+
+[RequireComponent(typeof(AuraLight))]
+public class AuraController : MonoBehaviour
+{
+    [SerializeField]
+    private AuraLight light;
+
+    [SerializeField]
+    private AnimationCurve lightCurve;
+
+    [SerializeField]
+    private float runRatio;
+
+    [SerializeField]
+    private Vector2 MinMax;
+
+    void Update()
+    {
+        light.strength = Mathf.Lerp(MinMax.x, MinMax.y, lightCurve.Evaluate(Mathf.Repeat(Time.time * runRatio, 1)));
+    }
+}
